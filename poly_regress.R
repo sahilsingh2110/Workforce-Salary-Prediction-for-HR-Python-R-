9d5db9f1cc47996e9dc6cd995e4dbcd87072cb99
Sahil Singh <40129527+sahilsingh2110@users.noreply.github.com>
2018-08-22 15:01:47 -0400

    Add files via upload

---
+++ poly_regress.R
@@ -0,0 +1,34 @@
+# Polynomial linear regression
+
+# Importing the dataset
+dataset = read.csv('Position_Salaries.csv')
+dataset = dataset[2:3]
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+#library(caTools)
+# set.seed(123)
+# split = sample.split(dataset$Salary, SplitRatio = 2/3)
+# training_set = subset(dataset, split == TRUE)
+# test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+# training_set = scale(training_set)
+# test_set = scale(test_set)
+
+# Fitting Linear Regression to the dataset
+library(ggplot2)
+lin_reg = lm(formula= Salary ~ ., data = dataset)
+# Visualizing the Linear Regression model
+ggplot() + geom_point(aes(x = dataset$Level, y= dataset$Salary), color = "red") + geom_line(aes(x = dataset$Level, y= predict(lin_reg, newdata = dataset)), color = "blue") + ggtitle("Truth or Bluff(LInear Regression)") + xlab("Level")+ ylab("Salary")
+
+y_pred = predict(lin_reg, data.frame(Level = 6.5))
+# Fitting Polynomial Regression to the dataset
+dataset$Level2 = dataset$Level^2
+dataset$Level3 = dataset$Level^3
+dataset$Level4 = dataset$Level^4
+poly_reg = lm(formula= Salary ~ ., data = dataset)
+# Visualizing the Polynomial Regression model
+ggplot() + geom_point(aes(x = dataset$Level, y= dataset$Salary), color = "red") + geom_line(aes(x = dataset$Level, y= predict(poly_reg, newdata = dataset)), color = "blue") + ggtitle("Truth or Bluff(POLY Regression)") + xlab("Level")+ ylab("Salary")
+
+y_pred = predict(poly_reg, data.frame(Level = 6.5,Level2 = 6.5^2,Level3=6.5^3,Level4=6.5^4))
---
+++ svr_regression.R
@@ -0,0 +1,34 @@
+# Polynomial linear regression
+
+# Importing the dataset
+dataset = read.csv('Position_Salaries.csv')
+dataset = dataset[2:3]
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+#library(caTools)
+# set.seed(123)
+# split = sample.split(dataset$Salary, SplitRatio = 2/3)
+# training_set = subset(dataset, split == TRUE)
+# test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+# training_set = scale(training_set)
+# test_set = scale(test_set)
+
+# Fitting the SVR to the dataset
+#install.packages('e1071')
+#library(e1071)
+regressor = svm(formula = Salary ~ .,
+                data = dataset,
+                type = 'eps-regression'
+                )
+
+# Predicting a new result
+y_pred = predict(regressor, data.frame(Level = 6.5))
+
+# Visualizing the SVR Regression model results
+library(ggplot2)
+ggplot() + geom_point(aes(x = dataset$Level, y= dataset$Salary), color = "red") + geom_line(aes(x = dataset$Level, y= predict(regressor, newdata = dataset)), color = "blue") + ggtitle("Truth or Bluff(SVR Regression)") + xlab("Level")+ ylab("Salary")
+
+
